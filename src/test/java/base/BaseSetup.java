caeca44a346267bb734f54c394116202429748bf
Ferdaws Sekander <Ferdawssek12@gmail.com>
2024-03-27 09:53:38 -0400

    My third changes

---
+++ src/test/java/base/BaseSetup.java
@@ -0,0 +1,16 @@
+package base;
+
+public class BaseSetup {
+
+    public static void main(String[] args) {
+
+        int numberOne = 2;
+        int numberTwo = 3;
+        int result;
+         result = numberOne * numberTwo;
+
+        System.out.println("The result of your calculation is = " +result);
+
+
+    }
+}
