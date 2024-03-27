15241f28afbac771125d21dcd7021df85ddeae4d
Ferdaws Sekander <Ferdawssek12@gmail.com>
2024-03-27 09:34:58 -0400

    My first Changes

---
+++ src/test/java/test/Mainmethod.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Mainmethod {
+
+//https://github.com/Ferdaws9894/tek-osprey-first-git.git
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
